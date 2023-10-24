4ebdf52c46605aa27f4b9b886755c7b2e037bd6f
KOPRajs <zdenek.koprivik@post.cz>
2023-10-24 12:00:24 +0200

    Add shaders for Retroplayer

---
+++ packages/mediacenter/kodi-binary-addons/game.shader.presets/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="game.shader.presets"
+PKG_VERSION="20.0.1-Nexus"
+PKG_SHA256="c68722f362cde1be96a287c0801d882cc5880c66ddd2576dd32f567194cddd00"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.shader.presets"
+PKG_URL="https://github.com/kodi-game/game.shader.presets/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="game.shader.presets: Shader preset support"
+PKG_LONGDESC="game.shader.presets adds libretro meta shader preset support"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
